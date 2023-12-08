9f2d1009077ad96910fd77033bfe96bb4b1e8966
Jose C. Massón <jose.masson@canonical.com>
2023-12-08 16:59:15 -0300

    let's start with a simpler version

--- src/mimir_coordinator.py
+++ src/mimir_coordinator.py
@@ -126,7 +126,7 @@ class MimirCoordinator:
                     "cert": self._tls_requirer.cert,
                     "key": self._tls_requirer.key,
                     "client_ca": self._tls_requirer.ca,
-                    "client_auth_type": "RequireAndVerifyClientCert",
+                    "client_auth_type": "NoClientCert",
                 },
                 "grpc_tls_config": {
                     "cert": self._tls_requirer.cert,
