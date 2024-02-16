827b3031b9e37bb55f742b9bd3af9047a83e32e9
Luca Bello <luca.bello@canonical.com>
2024-02-16 11:48:42 +0100

    de-bump mimir_cluster to 0.2 cuase we skipped that version

--- lib/charms/mimir_coordinator_k8s/v0/mimir_cluster.py
+++ lib/charms/mimir_coordinator_k8s/v0/mimir_cluster.py
@@ -24,7 +24,7 @@ LIBID = "9818a8d44028454a94c6c3a01f4316d2"
 DEFAULT_ENDPOINT_NAME = "mimir-cluster"
 
 LIBAPI = 0
-LIBPATCH = 3
+LIBPATCH = 2
 
 BUILTIN_JUJU_KEYS = {"ingress-address", "private-address", "egress-subnets"}
 
