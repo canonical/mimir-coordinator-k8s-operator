c5d5d0594c25701eea5bea86bea32e392de795ec
Sina <sina.pahlavan@canonical.com>
2025-06-25 20:51:34 -0400

    chore: add charm tests for exemplars

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -52,3 +52,33 @@ def test_recommended(mock_coordinator, roles, expected):
     mc.roles_config = MIMIR_ROLES_CONFIG
 
     assert mc.is_recommended is expected
+
+# Test workers_config_getter method
+@patch("coordinated_workers.coordinator.Coordinator.__init__", return_value=None)
+@pytest.mark.parametrize(
+    "max_global_exemplars_per_user, expected_value",
+    [
+        (None, 0),
+        (0, 0),
+        (-1, 0),
+        (50000, 100000),
+        (100000, 100000),
+        (150000, 150000),
+    ],
+)
+def test_workers_config_getter(mock_coordinator, max_global_exemplars_per_user, expected_value):
+    mc = Coordinator(None, None, "", "", 0, None, None, None)  # pyright: ignore
+    
+    def side_effect():
+        if max_global_exemplars_per_user is None or max_global_exemplars_per_user <= 0:
+            return 0
+        elif 1 <= max_global_exemplars_per_user <= 100000:
+            return 100000
+        else:
+            return max_global_exemplars_per_user
+    
+    mc._workers_config_getter = MagicMock(side_effect=side_effect)
+    
+    result = mc._workers_config_getter()
+    
+    assert result is expected_value
