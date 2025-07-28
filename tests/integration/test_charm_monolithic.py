81a8597320675486ac0ee585975bd65f995e70e4
Sina <sina.pahlavan@canonical.com>
2025-07-28 12:34:47 -0400

    add otel-col to deployed apps

--- tests/integration/test_charm_monolithic.py
+++ tests/integration/test_charm_monolithic.py
@@ -38,6 +38,7 @@ async def test_build_and_deploy(ops_test: OpsTest, mimir_charm: str, cos_channel
         ops_test.model.deploy("grafana-k8s", "grafana", channel=cos_channel, trust=True),
         ops_test.model.deploy("grafana-agent-k8s", "agent", channel=cos_channel),
         ops_test.model.deploy("traefik-k8s", "traefik", channel="latest/edge", trust=True),
+        ops_test.model.deploy("opentelemetry-collector-k8s", "otel-col", trust=True, channel=cos_channel),
         # Deploy and configure Minio and S3
         # Secret must be at least 8 characters: https://github.com/canonical/minio-operator/issues/137
         ops_test.model.deploy(
@@ -53,7 +54,7 @@ async def test_build_and_deploy(ops_test: OpsTest, mimir_charm: str, cos_channel
     await configure_s3_integrator(ops_test)
 
     await ops_test.model.wait_for_idle(
-        apps=["prometheus", "loki", "grafana", "minio", "s3"], status="active"
+        apps=["prometheus", "loki", "grafana", "minio", "s3", "otel-col"], status="active"
     )
     await ops_test.model.wait_for_idle(apps=["mimir", "agent"], status="blocked")
 
@@ -87,6 +88,7 @@ async def test_integrate(ops_test: OpsTest):
         ops_test.model.integrate("mimir:ingress", "traefik"),
         ops_test.model.integrate("mimir:receive-remote-write", "agent"),
         ops_test.model.integrate("agent:metrics-endpoint", "grafana"),
+        ops_test.model.integrate("mimir:receive-remote-write", "otel-col:send-remote-write"),
     )
 
     await ops_test.model.wait_for_idle(
